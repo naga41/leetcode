b18a08537c5d3ac8e6aa60ebe5906cb618357d60
Shuhei Nagata <shnagata.41@gmail.com>
2020-07-28 22:06:59 +0900

    Add test

--- Learn/Arrays101/Check_if_N_and_Its_Double_Exist/main.py
+++ Learn/Arrays101/Check_if_N_and_Its_Double_Exist/main.py
@@ -21,24 +21,3 @@ if __name__ == "__main__":
     nums = [10, 2, 5, 3]
     sol = Solution()
     print(sol.checkIfExist(nums))
-
-    nums = [7, 1, 14, 11]
-    print(sol.checkIfExist(nums))
-
-    nums = [3, 1, 7, 11]
-    print(sol.checkIfExist(nums))
-
-    nums = []
-    print(sol.checkIfExist(nums))
-
-    nums = [0]
-    print(sol.checkIfExist(nums))
-
-    nums = [2, 4, 6]
-    print(sol.checkIfExist(nums))
-
-    nums = [-2, 0, 10, -19, 4, 6, -8]
-    print(sol.checkIfExist(nums))
-
-    nums = [0, 0]
-    print(sol.checkIfExist(nums))
